1470c72e98e9ae5fe4d93136af3990bc0046c24d
s41173 <sanjaya.kiran@gmail.com>
2018-05-06 21:34:29 +0700

    bugs fixed12

--- www/js/acl.js
+++ www/js/acl.js
@@ -6,7 +6,37 @@ var api = "http://calculator.dswip.com/";
 
 function otentikasi(){
     
+    $(document).ready(function(){  
+    
+        alert("ini muncul setelah 3 detik");
+        // setInterval(function(){ 
+            
+        //     // var nilai = '{ "userid":"'+localStorage.userid+'", "log":"'+localStorage.log+'", "mobile":"1" }';
+        //     // $.ajax({
+        //     //     type: 'POST',
+        //     //     url: api+'agent/otentikasi',
+        //     //     data : nilai,
+        //     //     contentType: "application/json",
+        //     //     dataType: 'json',
+        //     //     success: function(data)
+        //     //     {   
+        //     //       if (data.status == false){ logout(); }
+        //     //     },
+        //     //     error: function (request, status, error) {
+        //     //         alert('Request Failed...!', error, "error");
+        //     //         console.log('Request Failed...!'+error);
+        //     //     }
+        //     // })
+        //     // return false; 
 
+        //     }, param);
+
+    }); // end document ready
+
+}
+
+function otentikasi_interval(){
+    
     alert("ini muncul setelah 3 detik");
 
     // $(document).ready(function(){  
