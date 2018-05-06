f29cbef7ba2a94621b7ae84ed6d7f1d9df987e00
s41173 <sanjaya.kiran@gmail.com>
2018-05-06 20:58:59 +0700

    bugs fixed6

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,8 @@
 var api = "http://calculator.dswip.com/";  
 
-function dodolx(){
-    alert("Hallo saya dodol");
-}
+function helloword(){
+    alert("Hello World");
+}  
 
 function otentikasi(param=6000){
     
