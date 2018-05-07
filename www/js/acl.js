c731e38f837671fa450c9d37506ff748866c5bf8
s41173 <sanjaya.kiran@gmail.com>
2018-05-07 13:32:06 +0700

    revision20

--- www/js/acl.js
+++ www/js/acl.js
@@ -18,7 +18,7 @@ function otentikasi(){
             success: function(data)
             {   
               if (data.status == false){ logout(); }
-              else if (data.status == true){ category(16); /* category(16); */ }
+              else if (data.status == true){ category(16); category(7); }
             },
             error: function (request, status, error) {
                 console.log('Request Failed...!'+error);
@@ -31,49 +31,6 @@ function otentikasi(){
 
     }); // end document ready
 
-}
-
-function category(param){
-       
-    alert("Saya adalah function category");
-
-    localStorage.removeItem("category");
-    localStorage.removeItem("catname");
-    localStorage.removeItem("model");
-    localStorage.removeItem("modelname");
-    localStorage.removeItem("pid");
-       
-    $(document).ready(function (e) {   
-    
-        var nilai = '{ "id":"'+param+'"}';
-        
-        $.ajax({
-            type: 'POST',
-            url: api+'api/category/',
-            data : nilai,
-            contentType: "application/json",
-            dataType: 'json',
-            success: function(data) 
-            {
-              for (i=0; i<data.content.length; i++){
-//                  alert(data.content[i].name.toUpperCase());
-                  var datax = data.content;
-                  var con = "<div class=\"col-xs-4\"> <div class=\"thumbnail shadow\">"+
-"<a onclick=\"series("+datax[i].id+",'"+datax[i].name+"');\" href=\"#\"> <img class=\"img-responsive\" id=\"pict\" src=\""+datax[i].image+"\" alt=\"\"> </a>"+
-                            "<div class=\"caption\"> <h5 class=\"judul strong\"> "+datax[i].name.toUpperCase()+" </h5>"+
-                            "</div> </div> </div>";
-                  if (param == 7){ $("#doorbox").append(con); }else{ $("#windowbox").append(con); }
-              }
-            },
-            error: function (request, status, error) {
-               alert('Request Failed Category...!'+error);
-                console.log('Request Failed Category...!');
-            }
-        }) 
-        return false;
-        
-    });  // end document ready	
-       
 }
 
 function logout(){
