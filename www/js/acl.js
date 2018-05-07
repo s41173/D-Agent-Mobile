10dbc3d02a11a99bd03a26a1180bede9d95c3c76
s41173 <sanjaya.kiran@gmail.com>
2018-05-07 09:31:46 +0700

    revision6

--- www/js/acl.js
+++ www/js/acl.js
@@ -5,24 +5,23 @@ function otentikasi(){
     
     $(document).ready(function(){  
             
-        alert(api);
-        // var nilai = '{ "userid":"'+localStorage.userid+'", "log":"'+localStorage.log+'", "mobile":"1" }';
-        // $.ajax({
-        //     type: 'POST',
-        //     url: api+'agent/otentikasi',
-        //     data : nilai,
-        //     contentType: "application/json",
-        //     dataType: 'json',
-        //     success: function(data)
-        //     {   
-        //       if (data.status == false){ logout(); }
-        //       else if (data.status == true){ category(7); category(16); }
-        //     },
-        //     error: function (request, status, error) {
-        //         console.log('Request Failed...!'+error);
-        //     }
-        // })
-        // return false;
+        var nilai = '{ "userid":"'+localStorage.userid+'", "log":"'+localStorage.log+'", "mobile":"1" }';
+        $.ajax({
+            type: 'POST',
+            url: api+'agent/otentikasi',
+            data : nilai,
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(data)
+            {   
+              if (data.status == false){ logout(); }
+              else if (data.status == true){ category(7); category(16); }
+            },
+            error: function (request, status, error) {
+                console.log('Request Failed...!'+error);
+            }
+        })
+        return false;
 
     }); // end document ready
 
