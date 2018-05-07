14e4a961cf971d8aadcdb9fa93199c81be31f4cf
s41173 <sanjaya.kiran@gmail.com>
2018-05-07 09:57:55 +0700

    revision9

--- www/js/acl.js
+++ www/js/acl.js
@@ -3,26 +3,30 @@ var api = "http://calculator.dswip.com/";
 // setInterval(otentikasi, 3000);
 function otentikasi(){
     
-    $(document).ready(function(){  
+    $(document).ready(function(e){  
+
+        // var userid = localStorage.getItem("userid");
+        // var log = localStorage.getItem("log");
             
         var nilai = '{ "userid":"'+localStorage.userid+'", "log":"'+localStorage.log+'", "mobile":"1" }';
-        $.ajax({
-            type: 'POST',
-            url: api+'agent/otentikasi',
-            data : nilai,
-            contentType: "application/json",
-            dataType: 'json',
-            success: function(data)
-            {   
-              if (data.status == false){ logout(); }
-              else if (data.status == true){ category(7); category(16); }
-            },
-            error: function (request, status, error) {
-                console.log('Request Failed...!'+error);
-                alert('Request Failed Otentikasi...! - '+error);
-            }
-        })
-        return false;
+        alert(nilai);
+        // $.ajax({
+        //     type: 'POST',
+        //     url: api+'agent/otentikasi',
+        //     data : nilai,
+        //     contentType: "application/json",
+        //     dataType: 'json',
+        //     success: function(data)
+        //     {   
+        //       if (data.status == false){ logout(); }
+        //       else if (data.status == true){ category(7); category(16); }
+        //     },
+        //     error: function (request, status, error) {
+        //         console.log('Request Failed...!'+error);
+        //         alert('Request Failed Otentikasi...! - '+error);
+        //     }
+        // })
+        // return false;
 
     }); // end document ready
 
